0c9c73c4e9d488ed4720fe6cc8e441562a6fca02
Artur Golubev <golubev.artur2021@gmail.com>
2026-04-30 17:17:15 +0200

    Навбар: обрезан пустой фон логотипа, убраны CSS-отступы

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -28,7 +28,7 @@ const Navbar = () => {
 
   return (
     <nav className="fixed top-0 left-0 right-0 z-50 bg-background/90 backdrop-blur-md border-b border-border">
-      <div className="container mx-auto flex items-center justify-between py-1 px-4 md:px-8">
+      <div className="container mx-auto flex items-center justify-between py-0 px-4 md:px-8">
         <Link to="/" className="inline-flex items-center self-center">
           <img src="/logo.png" alt="Голубев Консалтинг" className="h-40 w-auto" />
         </Link>
